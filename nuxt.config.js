287728f54b961c9ed9a0e18f99af65899bdfe9d7
ledinhhuan <ledinhhuan205@gmail.com>
2020-12-21 14:22:48 +0700

    update landing page

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['~/assets/styles/app.scss'],
+  css: ['~/styles/app.scss'],
 
   ssr: false,
   loadingIndicator: {
